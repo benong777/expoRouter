bfa29066cbd80ff7a79f85e8631c56e56dea3ca6
Ben Ong <ong.ben@gmail.com>
2024-12-29 23:16:03 -0800

    Added router back button for tab two

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router'
 
 const Two = () => {
+  const router = useRouter();
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 24 }}>Tab Two</Text>
+      <Button 
+        onPress={() => router.back()}
+        title='Go back'
+      />
     </View>
   )
 }
